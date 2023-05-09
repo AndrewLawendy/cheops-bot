5b2ec32ae34537d45d85ba5ed64bfc174ce06af2
AndrewLawendy <andrew.lawendy@outlook.com>
2023-05-09 12:43:06 +0300

    feat(send_message): :sparkles: add a post api for sending message

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
+from flask import Flask, request
 import os
+import logging
 import poe
+
+logging.basicConfig(level=logging.INFO)
+app = Flask(__name__)
 
 client = poe.Client(os.environ.get("POE_TOKEN"))
 
-for chunk in client.send_message("cheopsbot", "hello"):
-    print(chunk["text_new"], end="", flush=True)
+@app.route('/')
+def get_home():
+    return 'It works'
 
-for chunk in client.send_message("cheopsbot", "Travel to Cairo for 3 days"):
-    print(chunk["text_new"], end="", flush=True)
+@app.route('/send_message', methods = ["POST"])
+def send_message():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        json = request.get_json()
+        message = json["message"]
+        response = client.send_message("cheopsbot", message)
+        reply = ""
 
-for chunk in client.send_message("cheopsbot", "Tell me about Lotr"):
-    print(chunk["text_new"], end="", flush=True)
+        for chunk in response:
+            reply += chunk["text_new"]
+
+        return reply
+    else:
+        return 'Content-Type not supported!'
+
+if __name__ == '__main__':
+    # run app in debug mode on port 5000
+    app.run(debug=True, port=5000)
