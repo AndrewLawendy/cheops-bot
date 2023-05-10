5189225f748d0e96ca3d0716c114c74026ee62a8
AndrewLawendy <andrew.lawendy@outlook.com>
2023-05-10 15:47:08 +0300

    style: :art: format code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, request
+"""Serves as an api to communicate with CheopsBot on Poe"""
 import os
 import logging
+from flask import Flask, request
 import poe
 
 logging.basicConfig(level=logging.INFO)
@@ -8,14 +9,18 @@ app = Flask(__name__)
 
 client = poe.Client(os.environ.get("POE_TOKEN"))
 
-@app.route('/')
-def get_home():
-    return 'It works'
 
-@app.route('/send_message', methods = ["POST"])
+@app.route("/")
+def get_home():
+    """Just a sign that the server works"""
+    return "It works"
+
+
+@app.route("/send_message", methods=["POST"])
 def send_message():
-    content_type = request.headers.get('Content-Type')
-    if (content_type == 'application/json'):
+    """A method to communicate with CheopsBot on Poe"""
+    content_type = request.headers.get("Content-Type")
+    if content_type == "application/json":
         json = request.get_json()
         message = json["message"]
         response = client.send_message("cheopsbot", message)
@@ -25,9 +30,10 @@ def send_message():
             reply += chunk["text_new"]
 
         return reply
-    else:
-        return 'Content-Type not supported!'
 
-if __name__ == '__main__':
+    return "Content-Type not supported!"
+
+
+if __name__ == "__main__":
     # run app in debug mode on port 5000
     app.run(debug=True, port=5000)
